6efac40a7e692a6f2078a480fd6784407799c733
Vadim <vadtverdohlebov@mail.ru>
2022-12-06 12:09:09 +0300

    add mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
-import express from "express";
-import serverRouter from "./routes/servers.js";
+import express from 'express';
+import serverRouter from './routes/servers.js';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 3000;
 const HOST = 'localhost';
+const db = 'mongodb+srv://vadim_tverdokhlebov:Vado121212@cluster0.ccfvd.mongodb.net/node_fastfood?retryWrites=true&w=majority';
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log('Connected to DB'))
+  .catch((error) => console.log(error));
 
 app.use(cors());
 
@@ -12,7 +19,7 @@ app.use(express.static('public'));
 
 app.use('/api', serverRouter);
 
-app.listen(PORT, HOST, function () {
-    console.log(`Server listens http://${HOST}:${PORT}`);
-    
-  })
+app.listen(PORT, HOST, () => {
+  console.log(`Server listens http://${HOST}:${PORT}`);
+
+})
