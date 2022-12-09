0d1a0545309d2ee8447d86aad9779caa57104144
Vadim <vadtverdohlebov@mail.ru>
2022-12-09 11:27:51 +0300

    connect to db

--- backend/controllers/servers.js
+++ backend/controllers/servers.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import fs from 'fs';
 
 let dataProduct = fs.readFileSync('data.json');
--- frontend/src/scripts/api/getDataProduct.js
+++ frontend/src/scripts/api/getDataProduct.js
@@ -1,6 +1,6 @@
 export async function getDataProduct() {
 
-    const url = 'http://localhost:3000/api';
+    const url = 'http://localhost:3000/api/products';
     const response = await fetch(url);
     const json = await response.json();
     
