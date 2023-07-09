515bb2acd57cc36da447ebc3d0317a01fa2fdc2b
VadimTverdokhlebov <vadtverdohlebov@mail.ru>
2023-07-09 18:05:36 +0300

    remove dir scripts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import './index.css';
 import './index.html';
-import './scripts/components/sandwich-modal/modal-window/modal-window.js';
-import './scripts/components/product-cart/product-cart.js';
-import './scripts/components/main-menu/main-menu.js';
-import './scripts/components/current-products/current-products.js';
-import productReceived from './scripts/store/actionCreators/productReceived.js';
-import { storeDataProduct } from './scripts/store/store.js';
-import getDataProduct from './scripts/api/getDataProduct.js';
+import './components/sandwich-modal/modal-window/modal-window.js';
+import './components/product-cart/product-cart.js';
+import './components/main-menu/main-menu.js';
+import './components/current-products/current-products.js';
+import productReceived from './store/actionCreators/productReceived.js';
+import { storeDataProduct } from './store/store.js';
+import getDataProduct from './api/getDataProduct.js';
 
 async function uploadDataProductToStore() {
   const dataProduct = await getDataProduct();
